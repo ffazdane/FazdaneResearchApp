5f34bd29538704491dd7d4d0d5bc3d54e2200adb
ffazdane <ffazdane@github.com>
2026-08-17 17:04:16 -0500

    style: Increase dataframe height for screener results

--- modules/ticker_screener/screener_ui.py
+++ modules/ticker_screener/screener_ui.py
@@ -95,4 +95,4 @@ class TickerScreenerModule(FazDaneModule):
                         use_container_width=True
                     )
                     
-                st.dataframe(df, use_container_width=True, hide_index=True)
+                st.dataframe(df, use_container_width=True, hide_index=True, height=800)
